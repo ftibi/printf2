ace8134a47c1bf6caf2a87cce6eb870db2aeafad
Thibault FOLLY <tfolly@e3r5p9.42.fr>
2016-04-10 15:38:23 +0200

    proto

---
+++ includes/ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tfolly <tfolly@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/04/10 15:34:56 by tfolly            #+#    #+#             */
+/*   Updated: 2016/04/10 15:37:08 by tfolly           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF
+# define FT_PRINTF
+# include "../libft/includes/libft.h"
+# include <stdarg.h>
+
+int		ft_printf(const char *format, ...);
+
+
+#endif
