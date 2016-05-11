e47494eeca3578c01717c100ee2f29991e378e99
tfolly <tfolly@student.42.fr>
2016-05-11 13:56:57 +0200

    formatage

--- sources/formatage.c
+++ sources/formatage.c
@@ -23,7 +23,7 @@ int			lst_finish(t_lst *lst, t_data *data)
 	return (count);
 }
 
-t_lst		*format_lst(t_lst *lst, t_data *data) //relire la doc pour voir si jai oublie qqch
+t_lst		*format_lst(t_lst *lst, t_data *data)
 {
 	lst = hashfmt(lst, data);
 	lst = plusfmt(lst, data);
@@ -35,17 +35,16 @@ t_lst		*format_lst(t_lst *lst, t_data *data) //relire la doc pour voir si jai ou
 
 t_lst		*hashfmt(t_lst *lst, t_data *data)
 {
-	//ft_putendl("hashfmt")
 	if (data->hash)
 	{
-		if ((*(data->fmt) == 'o' || *(data->fmt) == 'O') && lst->c != '0')//&& data->precision != 0)//le premier char doit etre 0
-		lst = pushfront_lst(lst, '0');
-		if (*(data->fmt) == 'x' && lst->c != '0' && data->precision)  // data->precision inutile??
+		if ((*(data->fmt) == 'o' || *(data->fmt) == 'O') && lst->c != '0')
+			lst = pushfront_lst(lst, '0');
+		if (*(data->fmt) == 'x' && lst->c != '0' && data->precision)
 		{
 			lst = pushfront_lst(lst, 'x');
 			lst = pushfront_lst(lst, '0');
 		}
-		if (*(data->fmt) == 'X' && lst->c != '0' && data->precision) // data->precision inutile??
+		if (*(data->fmt) == 'X' && lst->c != '0' && data->precision)
 		{
 			lst = pushfront_lst(lst, 'X');
 			lst = pushfront_lst(lst, '0');
@@ -56,13 +55,12 @@ t_lst		*hashfmt(t_lst *lst, t_data *data)
 
 t_lst		*mwidthfmt(t_lst *lst, t_data *data)
 {
-	char    c;
-	t_lst		*tmp;
+	char	c;
 
-	if (data->zero && !data->minus) //possible de faire un ternaire sur 1 ligne
-	c = '0';
+	if (data->zero && !data->minus)
+		c = '0';
 	else
-	c = ' ';
+		c = ' ';
 	while (lst_len(lst) < data->mwidth)
 	{
 		if (data->precision > 0 && lst_digit(lst) >= data->precision && ft_strchr("idDxXoOuU", *(data->fmt)))
@@ -71,14 +69,10 @@ t_lst		*mwidthfmt(t_lst *lst, t_data *data)
 			lst = pushback_lst(lst, c);
 		else
 		{
-			if (lst && (lst->c == '-' || lst-> c == '+') && c == '0')
-			{
-				tmp = lst->next;
-				lst->next = new_lst(c);
-				lst->next->next = tmp;//possible de faire un push front sur le next en 1 ligne?
-			}
+			if (lst && (lst->c == '-' || lst->c == '+') && c == '0')
+				lst->next = pushfront_lst(lst->next, c);
 			else if ((*(data->fmt) == 'p' || ((*(data->fmt) == 'x' || *(data->fmt) == 'X') && data->hash)) && c == '0')
-					lst->next->next = pushfront_lst(lst->next->next, '0');
+				lst->next->next = pushfront_lst(lst->next->next, '0');
 			else
 				lst = pushfront_lst(lst, c);
 		}
@@ -93,19 +87,15 @@ t_lst		*precisionfmt(t_lst *lst, t_data *data)
 	int		count;
 
 	start = lst;
-	if (data->precision == 0 && ft_strchr(ft_strdup("idDoOuUxX"), *(data->fmt)) && !data->hash) //&& !data->l && !data->ll && !data->h && !data->hh && !data->j && !data->z)
+	if (data->precision == 0 && ft_strchr(ft_strdup("idDoOuUxX"), *(data->fmt)) && !data->hash && (lst_sum_digit(lst) == 0))
+		lst = del_all_digits(lst)	;
+	if (data->precision == 0 && ((ft_strchr(ft_strdup("xX"), *(data->fmt)) && data->hash) || (ft_strchr(ft_strdup("pP"), *(data->fmt)))))
 	{
-		if (lst_sum_digit(lst) == 0)
-		lst = del_all_digits(lst)	;// ne pas delete tous les 0 si # ATTENTION A CORRIGER
-	}
-
-	if (data->precision == 0 && ((ft_strchr(ft_strdup("xX"), *(data->fmt)) && data->hash) || (ft_strchr(ft_strdup("pP"), *(data->fmt))))) //&& !data->l && !data->ll && !data->h && !data->hh && !data->j && !data->z)
-	{
-		if (lst_sum_alnum(lst) == 0) //fonction a implementer pourles pointeurs
+		if (lst_sum_alnum(lst) == 0)
 		{
 			lst = del_all_digits(lst)	;
 			if (*(data->fmt) == 'p' || *(data->fmt) == 'P')
-			lst = pushfront_lst(lst, '0');
+				lst = pushfront_lst(lst, '0');
 		}
 	}
 	if (data->precision > 0 && ft_strchr(ft_strdup("S"), *(data->fmt)))
@@ -114,7 +104,7 @@ t_lst		*precisionfmt(t_lst *lst, t_data *data)
 	}
 	else if (data->precision >= 0 && ft_strchr(ft_strdup("sS"), *(data->fmt)))
 	{
-		if(!data->precision)
+		if (!data->precision)
 		{
 			free(lst);
 			return (0);
@@ -139,10 +129,7 @@ t_lst		*precisionfmt(t_lst *lst, t_data *data)
 		if (*(data->fmt) == 'p' || (data->hash && ((*(data->fmt) == 'x' || *(data->fmt) == 'X'))))
 		{
 			while (data->precision > lst_len(lst) - 2)
-			{
-				//  ft_putendl("ici");
 				lst->next->next = pushfront_lst(lst->next->next, '0');
-			}
 		}
 		if (ft_strchr(ft_strdup("idDxXoOuU"), *(data->fmt)))
 		{
@@ -154,18 +141,14 @@ t_lst		*precisionfmt(t_lst *lst, t_data *data)
 					lst = pushfront_lst(lst, '0');
 			}
 		}
-
 	}
 	return (lst);
 }
 
 t_lst		*plusfmt(t_lst *lst, t_data *data)
 {
-	if (data->plus && ft_strchr("idD",*(data->fmt)))
-	{
-		if (lst->c != '-')// && lst->c != '0')
+	if (data->plus && ft_strchr("idD", *(data->fmt)) && (lst->c != '-'))
 		lst = pushfront_lst(lst, '+');
-	}
 	return (lst);
 }
 
@@ -174,11 +157,11 @@ t_lst		*spacefmt(t_lst *lst, t_data *data)
 	t_lst	*start;
 
 	if (!lst)
-	return (0);
+		return (0);
 	if (!data->space || data->plus)
-	return (lst);
+		return (lst);
 	start = lst;
-	if (ft_strchr("idD",*(data->fmt)))
+	if (ft_strchr("idD", *(data->fmt)))
 	{
 		if (ft_isdigit(lst->c))
 		{
